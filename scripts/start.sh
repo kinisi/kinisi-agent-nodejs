a522b8cacb07be0e357f743663606dbfae4ab980
Jeffrey Yang <jeffrey.a.yang@gmail.com>
2014-02-15 10:30:21 -0500

    Ensure log directory exists in start script.

--- scripts/start.sh
+++ scripts/start.sh
@@ -2,6 +2,12 @@
 LOGDIR=~/.kinisiagent/logs
 SCRIPTDIR=$(dirname "$0")
 
+# Ensure log directory
+if [ ! -d $LOGDIR ]; then 
+    mkdir -p $LOGDIR
+fi
+
+# Change to app directory first (for Node.JS, relative paths are important)
 pushd $SCRIPTDIR/..
   (nohup node kinisi-agent.js > $LOGDIR/kinisi.log 2>&1 )&
 popd
