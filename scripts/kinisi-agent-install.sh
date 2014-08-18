0693c9bbc61dc89c8ea85518c63dac023c9463a9
aeratek <aeratek@users.noreply.github.com>
2014-08-18 17:52:38 -0400

    Create kinisi-agent-install.sh
    Helper installation script for the Kinisi Nodejs agent. This script assumes that the tarred code is already present as /usr/local/master.tar.gz

---
+++ scripts/kinisi-agent-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# This is a helper script for installing the Kinisi agent
+# This script assumes that the tarred code is already present as /usr/local/master.tar.gz
+cd /usr/local
+tar xzf master.tar.gz
+mv kinisi-agent-nodejs-master kinisiagent-nodejs
+cd kinisi-agent-nodejs
+# Install NPM dependencies
+npm install
+# Setup some directories
+chmod -R 777 config
+mkdir -p /var/log/kinisiagent/archive
+mkdir -p /usr/local/kinisi-agent/nodejs/run/commit
+# Install Service
+cp /usr/local/kinisi-agent-nodejs/scripts/kinisiagent /etc/init.d/kinisiagent
+chmod 755 /etc/init.d/kinisiagent
+update-rd.d kinisiagent defaults
+echo Done with Kinisi Agent Install Script
